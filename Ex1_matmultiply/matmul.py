78a48d14a95d4750c2e7414c2c6bb13ea4b58bc1
Ilker YILMAZ <ilker.ned@gmail.com>
2022-10-03 21:25:23 +0200

    Experiment with Numpy and Matrices

--- Ex1_matmultiply/matmul.py
+++ Ex1_matmultiply/matmul.py
@@ -1,31 +1,76 @@
 import random
 import numpy as np
 
-matrix1 = np.random.randint(3, size=(3,3))
-matrix2 = np.random.randint(3, size=(3,3))
-matrix4 = np.zeros((3,3))
+''' Implementation without numpy library'''
 rows = 3
 columns = 3
-matrix3 = [[random.randrange(0, 10) for x in range(columns)] for y in range(rows)]
+matrix1 = [[0,0,0],
+           [0,0,0],
+           [0,0,0]]
+matrix2 = [[0,0,0],
+           [0,0,0],
+           [0,0,0]]
+result = [[0,0,0],
+          [0,0,0],
+          [0,0,0]]
+result2 = [[0,0,0],
+          [0,0,0],
+          [0,0,0]]
+
 
 for x in range(columns):
 	for y in range(rows):
-		matrix4[x][y] = random.randrange(0,10)
+		matrix1[x][y] = random.randrange(0,10); matrix2[x][y] = random.randrange(0,10)
 
-print("------ Z -----")
-print(matrix4)
+# Matrix multiplication has been performed in following for-loop
 
-print(matrix3)
-
-print(matrix3[2][0])
-print("-------------")
-
-print("----  X  -----")
+for i in range(len(matrix1)):
+      # iterate through columns of Y
+      for j in range(len(matrix2[0])):
+        # iterate through rows of Y
+        for k in range(len(matrix2)):
+           result[i][j] += matrix1[i][k] * matrix2[k][j]
 print(matrix1)
-print("----  Y  -----")
 print(matrix2)
-print("-------------!")
+print(result)
+
+
+
+# matmul() function performs Matrix Multiplication
+
+def matmul(rows,columns):
+   for x in range(columns):
+	   for y in range(rows):
+		   matrix1[x][y] = random.randrange(0,10); matrix2[x][y] = random.randrange(0,10)
+   
+   for i in range(len(matrix1)):
+      # iterate through columns of Y
+      for j in range(len(matrix2[0])):
+        # iterate through rows of Y
+        for k in range(len(matrix2)):
+           result2[i][j] += matrix1[i][k] * matrix2[k][j]
+           return print(result)
+
+print("Function call")
+print(matrix1)
+print(matrix2)
+matmul(3,3)
+print("--------------")
+
+
+''' Implementation with numpy library
+    Let's initialize 3x3 matrices with
+    random integers range 0 to 9
+'''
+
+matrix1 = np.random.randint(10, size=(3,4))
+matrix2 = np.random.randint(10, size=(4,3))
 result = np.zeros((3,3))
+
+
+print("--------matrixes with numpy------")
+print(matrix1)
+print(matrix2)
 
 for i in range(len(matrix1)):
    # iterate through columns of Y
@@ -36,5 +81,5 @@ for i in range(len(matrix1)):
 
 for r in result:
    print(r)
-print("------------")
+# print("------------")
 print(result)
